76b9ced7f43ced04343e3851e22322b192c236cc
TeamKillerX <killerx@randydev.my.id>
2022-12-16 01:53:40 +0700

    Create __main__.py

---
+++ KillerXBase/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from KillerXBase.helper import join
+from KillerXBase.modules import ALL_MODULES
+from KillerXBase import clients, app, ids
+
+async def start_bot():
+    await app.start()
+    print("LOG: Mendirikan Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("KillerXBase.modules" + all_module)
+        print(f"Successfully Imported {all_module} 🛠️")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"Started {ex.first_name} 🛠️")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
