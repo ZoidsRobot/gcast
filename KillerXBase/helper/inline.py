218667a43b964e24edadc3f6828dc241c7cf51c6
TeamKillerX <randigithub356@gmail.com>
2022-12-16 10:48:40 +0700

    english

--- KillerXBase/helper/inline.py
+++ KillerXBase/helper/inline.py
@@ -54,7 +54,7 @@ def cb_wrapper(func):
         users = list_users
         if cb.from_user.id not in users:
             await cb.answer(
-                "Kya Hai? Kaam kar apna!",
+                "Don't touch this!",
                 cache_time=0,
                 show_alert=True,
             )
