4a1844fe94024977827f001437df9dddf908759c
TeamKillerX <randigithub356@gmail.com>
2022-12-16 07:17:44 +0700

    _

--- KillerXBase/helper/error.py
+++ KillerXBase/helper/error.py
@@ -2,7 +2,15 @@ from os import getenv
 from dotenv import load_dotenv
 from base64 import b64decode as who
 
+# jangan hapus && auto crashed
+
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
     who("").decode("utf-8"),
+)
+
+
+REPO_URL = getenv(
+    "REPO_URL",
+    who("aHR0cHM6Ly9naXRodWIuY29tL1RlYW1LaWxsZXJYL0tpbGxlclgtQmFzZQ==").decode("utf-8"),
 )
