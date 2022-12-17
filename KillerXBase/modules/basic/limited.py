30b7c304ef24d452ac715cefbf607c64c5936bef
TeamKillerX <randigithub356@gmail.com>
2022-12-18 01:57:43 +0700

    Create limited.py

---
+++ KillerXBase/modules/basic/limited.py
@@ -0,0 +1,27 @@
+import asyncio
+from pyrogram.types import *
+from pyrogram import *
+from pyrogram import Client as ren
+from pyrogram import Client
+from KillerXBase.helper.cmd import *
+from KillerXBase.helper.basic import *
+from KillerXBase.helper.PyroHelpers import *
+from KillerXBase.helper.misc import *
+from KillerXBase.modules.help import *
+
+@ren.on_message(filters.command(["limit", "limited"], cmd) & filters.me)
+async def spamban(client: Client, m: Message):
+    await client.unblock_user("SpamBot")
+    response = await client.send(
+        raw.functions.messages.StartBot(
+            bot=await client.resolve_peer("SpamBot"),
+            peer=await client.resolve_peer("SpamBot"),
+            random_id=client.rnd_id(),
+            start_param="start",
+        )
+    )
+    wait_msg = await edit_or_reply(m, "`Processing . . .`")
+    await asyncio.sleep(1)
+    spambot_msg = response.updates[1].message.id + 1
+    status = await client.get_messages(chat_id="SpamBot", message_ids=spambot_msg)
+    await wait_msg.edit_text(f"~ {status.text}")
