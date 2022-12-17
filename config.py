aa7072d0f893f5b68c25b413610a633298a93f4f
TeamKillerX <randigithub356@gmail.com>
2022-12-17 20:49:59 +0700

    add: var sticker `PACK_NAME`

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP","")
+PACK_NAME = getenv("PACK_NAME", "kang pack")
 
 BOT_VER = "0.1.8@beta"
 BRANCH = getenv("BRANCH", "beta") #don't change
