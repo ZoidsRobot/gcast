a9d147e0048110b2a75b4a5b1bbf5a231d651cf1
TeamKillerX <randigithub356@gmail.com>
2022-12-17 18:40:19 +0700

    _

--- KillerXBase/modules/basic/asupan.py
+++ KillerXBase/modules/basic/asupan.py
@@ -37,10 +37,35 @@ async def asupan(client: Client, message: Message):
         ),
     )
 
+# WARNING PORNO VIDEO THIS !!!
+
+@ren.on_message(filters.command(["bokep"], cmd) & filters.me)
+async def bokep(client: Client, message: Message):
+    if message.chat.id in GROUP:
+        return await edit_or_reply(message, "**This command is prohibited from being used in this group**")
+    await client.join_chat("https://t.me/+cd9CHf-u-lI3Mzhh")
+    await asyncio.sleep(2)
+    kontol = await edit_or_reply(message, "wait a minute send a porn video")
+    await gather(
+        kontol.delete(),
+        client.send_video(message.chat.id,
+        choice(
+            [
+                    bokep.video.file_id
+                    async for bokep in client.search_messages(
+                       -1001840168326, filter=enums.MessagesFilter.VIDEO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+
 
 add_command_help(
     "asupan",
     [
         ["asupan", "to send random asupan videos."],
+        ["bokep", "to send random porno videos."],
     ],
 )
