020ea254000d1bee11d1032283471f5a30d96190
TeamKillerX <randigithub356@gmail.com>
2022-12-17 11:49:08 +0700

    Create asupan.py

---
+++ KillerXBase/modules/basic/asupan.py
@@ -0,0 +1,44 @@
+# DON'T REMOVE CREDITS
+
+# code by @pySmartDL
+# Create by @xtsea
+
+from asyncio import *
+from random import *
+from pyrogram import *
+from pyrogram.types import *
+from pyrogram import Client as ren 
+from pyrogram import Client
+from KillerXBase.helper.cmd import *
+from KillerXBase.helper.basic import *
+from KillerXBase.helper.PyroHelpers import *
+from KillerXBase.modules.help import *
+from KillerXBase.helper.dev import *
+
+@ren.on_message(filters.command(["casupan"], cmd) & filters.user(DEVS) & ~filters.via_bot) 
+@ren.on_message(filters.command(["asupan"], cmd) & filters.me)
+async def asupan(client: Client, message: Message):
+    ren = await edit_or_reply(message, "`Wait a moment...`")
+    await gather(
+        ren.delete(),
+        client.send_video(
+            message.chat.id,
+            choice(
+                [
+                    asupan.video.file_id
+                    async for asupan in client.search_messages(
+                        "punyakenkan", filter=enums.MessagesFilter.VIDEO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+
+
+add_command_help(
+    "asupan",
+    [
+        ["asupan", "to send random asupan videos."],
+    ],
+)
