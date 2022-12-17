ddd06c61d4463c23256f5d93b33034691b5afe3a
TeamKillerX <randigithub356@gmail.com>
2022-12-18 04:50:07 +0700

    _

--- KillerXBase/modules/basic/limited.py
+++ KillerXBase/modules/basic/limited.py
@@ -11,14 +11,20 @@ from KillerXBase.modules.help import *
 
 @ren.on_message(filters.command(["limit", "limited"], cmd) & filters.me)
 async def spamban(client: Client, m: Message):
-    await client.unblock_user("spambot")
-    start = "/start"
-    bot = "@spambot"
-    await client.send_message(bot, start)
-    await asyncio.sleep(2)
-    async for test in client.get_chat_history(bot, 1):
-       await test.copy(m.id)
-       await test.delete()
+    await client.unblock_user("SpamBot")
+    response = await client.send(
+        raw.functions.messages.StartBot(
+            bot=await client.resolve_peer("SpamBot"),
+            peer=await client.resolve_peer("SpamBot"),
+            random_id=client.rnd_id(),
+            start_param="start",
+        )
+    )
+    wait_msg = await edit_or_reply(m, "`Processing . . .`")
+    await asyncio.sleep(1)
+    spambot_msg = response.updates[1].message.id + 1
+    status = await client.get_messages(chat_id="SpamBot", message_ids=spambot_msg)
+    await wait_msg.edit_text(f"~ {status.text}")
 
 add_command_help(
     "limited",
