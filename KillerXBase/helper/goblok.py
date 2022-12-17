0064aad34a2a709a814b102a2d72b6fa8a038b61
TeamKillerX <randigithub356@gmail.com>
2022-12-17 23:14:30 +0700

    _

--- KillerXBase/helper/goblok.py
+++ KillerXBase/helper/goblok.py
@@ -4,4 +4,40 @@ bokep = ""
 bocil = ""
 tiktok = ""
 
+absen = [
+    "**Hadir bang** 😁",
+    "**Hadir kak** 😉",
+    "**Hadir dong** 😁",
+    "**Hadir ganteng** 🥵",
+    "**Hadir bro** 😎",
+    "**Hadir dick** 🤣",
+    "**Hadir kak maap telat** 🥺",
+]
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["Dtk", "Mnt", "Jam", "Hari"]
+
+    while count < 4:
+        count += 50
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
 # BAYAR 25K PC @xtsea
