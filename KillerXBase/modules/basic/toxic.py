58fe19bd46b850bf6e04c51ec814f6f6b6ad49c2
TeamKillerX <randigithub356@gmail.com>
2022-12-17 14:28:05 +0700

    _

--- KillerXBase/modules/basic/toxic.py
+++ KillerXBase/modules/basic/toxic.py
@@ -8,6 +8,7 @@ from KillerXBase.helper.basic import *
 from KillerXBase.helper.adminHelpers import *
 from KillerXBase.helper.cmd import *
 from KillerXBase.helper.dev import *
+from KillerXBase.modules.help import *
 
 @ren.on_message(filters.command("pp", cmd) & filters.me)
 async def ppkntl(client: Client, message: Message):
@@ -15,3 +16,11 @@ async def ppkntl(client: Client, message: Message):
     if user_id in DEVS:
         return await edit_or_reply(message, "**This command is prohibited to use to my developers**")
     await asyncio.gather(message.delete(), client.send_message(message.chat.id, "PASANG PP DULU GOBLOK,BIAR ORANG-ORANG PADA TAU BETAPA HINA NYA MUKA LU 😆", reply_to_message_id=ReplyCheck(message),),)
+
+
+add_command_help(
+    "toxic",
+    [
+        ["pp", "ngenyek telegram jamet sing ora nganggo foto profil"],
+    ],
+)
