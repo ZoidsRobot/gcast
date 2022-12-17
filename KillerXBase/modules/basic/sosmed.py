4b9040b770cc58d24c027eac33f84a35711bb7ca
TeamKillerX <randigithub356@gmail.com>
2022-12-18 01:38:19 +0700

    _

--- KillerXBase/modules/basic/sosmed.py
+++ KillerXBase/modules/basic/sosmed.py
@@ -6,9 +6,10 @@ from pyrogram import Client
 from KillerXBase.helper.cmd import *
 from KillerXBase.helper.basic import *
 from KillerXBase.helper.PyroHelpers import *
+from KillerXBase.helper.misc import *
 
 @ren.on_message(filters.command(["tt", "tiktok", "ig", "sosmed"], cmd) & filters.me)
-async def tiktok(client: Client, message: Message):
+async def sosmed(client: Client, message: Message):
     prik = await message.edit("`Processing . . .`")
     link = get_arg(message)
     bot = "thisvidbot"
@@ -22,7 +23,7 @@ async def tiktok(client: Client, message: Message):
             tuyul = await client.send_message(bot, link)
             await asyncio.sleep(5)
             await tuyul.delete()
-    async for tiktok in client.search_messages(
+    async for sosmed in client.search_messages(
         bot, filter=enums.MessagesFilter.VIDEO, limit=1
     ):
         await asyncio.gather(
@@ -35,3 +36,10 @@ async def tiktok(client: Client, message: Message):
             ),
         )
         await client.delete_messages(bot, 2)
+
+add_command_help(
+    "sosmed",
+    [
+        [f"sosmed <link>", ", to download media from Facebook/Tiktok/Instagram/Twitter/Youtube",],
+    ],
+)
