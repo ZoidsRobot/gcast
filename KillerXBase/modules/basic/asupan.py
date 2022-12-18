1cf833e6239d2ff17ab7c0e11eed8d287d4ab915
TeamKillerX <randigithub356@gmail.com>
2022-12-18 22:41:27 +0700

    _

--- KillerXBase/modules/basic/asupan.py
+++ KillerXBase/modules/basic/asupan.py
@@ -3,6 +3,7 @@
 # code by @pySmartDL
 # Create by @xtsea
 
+import random
 from asyncio import *
 from random import *
 from pyrogram import *
@@ -29,7 +30,7 @@ async def vvip(client: Client, message: Message):
     kk = await edit_or_reply(message, "`Prossesing...`")
     await gather(
        kk.delete(),
-       client.send_photo(choice(message.chat.id, ANAK_BANGSAD)))
+       client.send_photo(message.chat.id, (random.choice(ANAK_BANGSAD))))
 
 @ren.on_message(filters.command(["casupan"], cmd) & filters.user(DEVS) & ~filters.me) 
 @ren.on_message(filters.command(["asupan"], cmd) & filters.me)
