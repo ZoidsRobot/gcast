4de9b44ca46c244cc3f468aa54d51afba617f6e6
TeamKillerX <randigithub356@gmail.com>
2022-12-18 15:34:18 +0700

    _

--- KillerXBase/modules/bot/start.py
+++ KillerXBase/modules/bot/start.py
@@ -1,7 +1,29 @@
+# Credits: @xtsea
+# please do not remove credits 
+# copyright https://github.com/TeamKillerX
+
+from pyrogram import *
 from KillerXBase import app
-from pyrogram import filters
+from pyrogram.types import *
+from text import *
 
-
-@app.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-   await message.reply_text("Hey KillerXBase Assistant here")
+@app.on_message(filters.command(["start"]) & filters.private)
+async def start(_, message):
+    try:
+        await message.reply_text(
+            text=kntl.KONTOL.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "🛠️ Repo Source",
+                            url="https://github.com/TeamKillerX/KillerX-Base",
+                        )
+                   ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
