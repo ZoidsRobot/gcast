320b2f245ad82b9f2b38acf15faaf032913d6299
TeamKillerX <randigithub356@gmail.com>
2022-12-18 15:35:30 +0700

    Create text.py

---
+++ text.py
@@ -0,0 +1,10 @@
+class kntl(object):
+
+     KONTOL = """Hey there! {}
+
+I'm an <b>administration</b> bot that helps you to keep your groups safe from <b>spammers</b>
+
+If you want to use me for your groups, note that I'm more useful on a pyrogram of groups and you also need to <b>setup a new bot.</b>
+
+So if you don't wish to self-host, @MissRose_bot might be a better choice for you.
+"""
