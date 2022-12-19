6bdcb2807bc463b29919aa07e5a1437e8c3a3245
TeamKillerX <randigithub356@gmail.com>
2022-12-20 03:14:26 +0700

    Create take.py

---
+++ KillerXBase/modules/basic/take.py
@@ -0,0 +1,29 @@
+import asyncio
+from pyrogram.types import *
+from pyrogram import *
+from pyrogram import Client as ren
+from pyrogram import Client
+from KillerXBase.helper.cmd import *
+from KillerXBase.helper.basic import *
+from KillerXBase.helper.PyroHelpers import *
+from KillerXBase.helper.misc import *
+from KillerXBase.modules.help import *
+
+@ren.on_message(filters.command(["take", cmd) & filters.me)
+async def take(client: Client, message: Message):
+    lol = await message.edit("`Prossing...`")
+    take = get_arg(message)
+    bot = ""
+    if link:
+        try:
+           a = await client.send_message(bot, link)
+           await asyncio.sleep(3)
+           await a.delete()
+        except YouBlockedUser:
+            await client.unblock_user(bot)
+            b = await client.send_message(bot, link)
+            await asyncio.sleep(5)
+            await b.delete()
+    async for i in client.get_chat_history(bot, 1):
+        await i.copy(message.chat.id)
+        await i.delete()
