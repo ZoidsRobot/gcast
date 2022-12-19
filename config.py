b70412fc00a39c7eb7cf2671a5c05b21d7fa34cc
TeamKillerX <randigithub356@gmail.com>
2022-12-19 16:36:02 +0700

    _

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ LOG_GROUP = int(getenv("LOG_GROUP", ""))
 PACK_NAME = getenv("PACK_NAME", "kang pack")
 
 # don't kanger repo this !!!
-CHANNEL = "RendyProjects"
-SUPPORT = "pantekyks"
+CHANNEL = "@RendyProjects"
+SUPPORT = "@pantekyks"
 
 BOT_VER = "0.2.0@dev"
 BRANCH = getenv("BRANCH", "dev") #don't change
