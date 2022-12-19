01b7672e36215fdb082145498215ffbca21d9912
TeamKillerX <randigithub356@gmail.com>
2022-12-19 22:21:25 +0700

    fixed : added module naruto

--- KillerXBase/modules/basic/naruto.py
+++ KillerXBase/modules/basic/naruto.py
@@ -12,12 +12,29 @@ from KillerXBase.helper.dev import *
 from KillerXBase.helper.misc import *
 from KillerXBase.helper.goblok import *
 
+# code by @xtsea
+
 @ren.on_message(filters.command("naruto", cmd) & filters.me)
 async def naruto(client: Client, message: Message):
-    yesus = await client.send_sticker(message.chat.id, "CAACAgIAAx0EXYGnZgACgv9joHVJOT943wxKO-DRZ9ZimQYTtwACOwAD9PRuF1nL4v7O2YZqHgQ")
-    try:
-       await asyncio.sleep(1.5)
-       await yesus.delete()
-    except BaseException:
-        pass
-    await client.send_photo(message.chat.id, "https://user-images.githubusercontent.com/90479255/208452872-b6067a9c-43c2-4171-9e2f-bc1bc09208c7.jpg")
+    a = await client.send_sticker(message.chat.id, "CAADBQADEwIAAkceYVabauGjzQ4OUgI")
+    await asyncio.sleep(1)
+    await a.delete()
+    e = await client.send_sticker(message.chat.id, "CAADBQADeQMAAj28YFb0CAvlSjuV5gI")
+    await asyncio.sleep(1)
+    await e.delete()
+    b = await client.send_sticker(message.chat.id, "CAADBQADPwIAAlyRYVamvYbqTt3XjQI")
+    await asyncio.sleep(1)
+    await b.delete()
+    c = await client.send_sticker(message.chat.id, "CAADBQADlAIAAnW1WVaorC7sMkfgKQI")
+    await asyncio.sleep(1)
+    await c.delete()
+    d = await client.send_sticker(message.chat.id, "CAADAgADXwAD9PRuFwpjuaddBSAcAg")
+    await asyncio.sleep(1)
+    await d.delete()
+    f = await client.send_sticker(message.chat.id, "CAADBQADdgIAAg62YVZOh1Z4kDVrfQI")
+    await asyncio.sleep(1)
+    await f.delete()
+    p = await client.send_message(message.chat.id, "summoning jutsu!!!!!!!!!")
+    await asyncio.sleep(1)
+    await p.delete()
+    await client.send_photo(message.chat.id, "https://telegra.ph/file/78c05f1d42e16c61990ca.jpg")
