d7c66376150e480c11b086af8e865bae77e45e4f
TeamKillerX <randigithub356@gmail.com>
2022-12-19 13:50:35 +0700

    _

--- KillerXBase/modules/basic/toxic.py
+++ KillerXBase/modules/basic/toxic.py
@@ -20,6 +20,42 @@ async def ppkntl(client: Client, message: Message):
         return await edit_or_reply(message, "**This command is prohibited from being used in this group**")
     await asyncio.gather(message.delete(), client.send_message(message.chat.id, "PASANG PP DULU GOBLOK,BIAR ORANG-ORANG PADA TAU BETAPA HINA NYA MUKA LU 😆", reply_to_message_id=ReplyCheck(message),),)
 
+@ren.on_message(filters.command("kont", cmd) & filters.me)
+async def toxic1(client: Client, message: Message):
+    user_id = await extract_user(message)
+    if user_id in DEVS:
+        return await edit_or_reply(message, "**This command is prohibited to use to my developers**")
+    toxic = await edit_or_reply(message, "**KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**LU ANAK KONTOL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**DI BIKIN DARI KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**MUKALU PERSIS KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**DASAR ANAK NGONTOLLLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**NOLEP KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**NGERUSUH KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**BENER BENER KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**PADAHAL LO GAPUNYA KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**MENDING LO OPERASI KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**BIAR LO PUNYA KONTOLL**")
+    await asyncio.sleep(1.5)
+    await toxic.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
+    
+
+@ren.on_message(filters.command("an", cmd) & filters.me)
+async def toxic2(client: Client, message: Message):
+    user_id = await extract_user(message)
+    if user_id in DEVS:
+        return await edit_or_reply(message, "")
+    await asyncio.gather(message.delete(), client.send_message(message.chat.id, "**LO KAN HASIL ZINAH HASIL HUB TERLARANG NAH LO DI BUANG DI TONGSAMPAH NAH MAK LO YG SKRG KASIAN SAMA LO MKNY DI PUNGUT UDH LO CACAT KAKI CACAT TANGAN CACAT MUKA CACAT SEMUAHNY PARAH BET SI KONTOL LO JUGA CACATAN PASTI NANAHAN JUGA,UDH MENDING LO BUNDIR DEH JADI BEBAN ORG DOANG BEGO NGERUGIN MASYARKAT BEGO BOCAH HINA BOCAH HARAM BOCAH AUTIS KEK LO MENDING MATI AJA**", reply_to_message_id=ReplyCheck(message),),)
 
 add_command_help(
     "toxic",
