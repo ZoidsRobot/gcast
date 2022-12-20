b9d18b12705c31c688cb22b4737f0d268ff08c5e
TeamKillerX <randigithub356@gmail.com>
2022-12-20 23:50:31 +0700

    _

--- KillerXBase/modules/basic/ping.py
+++ KillerXBase/modules/basic/ping.py
@@ -126,12 +126,14 @@ async def pingme_2(client: Client, message: Message):
 
 
 # by @xtsea
-@ren.on_message(filters.command("absen", cmd) & filters.user(DEVS) & ~filters.me)
+@ren.on_message(filters.command("cabsen", cmd) & filters.user(DEVS) & ~filters.me)
+@ren.on_message(filters.command("absen", cmd) & filters.me)
 async def absen(_, message: Message):
     await message.reply_text(choice(absen))
 
 # by @xtsea
-@ren.on_message(filters.command("spd", cmd) & filters.user(DEVS) & ~filters.me)
+@ren.on_message(filters.command("cspd", cmd) & filters.user(DEVS) & ~filters.me)
+@ren.on_message(filters.command("spd", cmd) & filters.me)
 async def absen(_, message: Message):
     await message.reply_text(choice(memek))
 
